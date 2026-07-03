13ffc689594f5f18c33e7e88a29c0605bf8cf4eb
msqcodes <ceo.msq@gmail.com>
2026-07-03 22:58:49 +0530

    new reactapp

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = ()=>{
+	return ( 
+<>
+		<div>shaik sameer anwar is learning react </div>
+		  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+</>
+	)
+}
+export default App
